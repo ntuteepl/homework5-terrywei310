dac2bfe449446263fefa891158e25e9296f6c962
Wei jung ting <132579810+terrywei310@users.noreply.github.com>
2023-11-06 23:12:17 +0800

    Update hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -1,24 +1,13 @@
 #include <stdio.h>
-
+#include <math.h>
 int main() {
     float C;
     printf("請輸入攝氏溫度：");
     scanf("%f", &C);
-
-    // 將攝氏溫度轉換為華氏溫度，保留一位小數
-    double F = ((C * 9.0 / 5.0) + 32.0);
-    // 取得小數部分
-    double decimal = F - (int)F;
-    // 如果小數部分大於等於0.5，則進位
-    if (decimal >= 0.5) {
-        F = (int)F + 1;
-    } else {
-        F = (int)F;
-    }
-
-    // 以 float 計算 ex
+    double f = (C * 9.0 /5.0) + 32;
+    double F = round(f * 10) / 10.0;
     printf("%.2f℉\n", F);
-    
     return 0;
 }
+
 
