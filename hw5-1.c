6740d12a8e4f9d7867dcff47937c010685dc8cab
Wei jung ting <132579810+terrywei310@users.noreply.github.com>
2023-11-06 22:32:11 +0800

    Update hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 #include <math.h>
-
-int main() {
-    float C;
-    printf("請輸入攝氏溫度：");
-    scanf("%f", &C);
-    
-    double F = roundf((C * 9.0 / 5.0 + 32.0) * 10) / 10.0;
-    
-    // 直接輸出 F，不需要進行額外的四捨五入
-    printf("%.1f℉", F);
-    
-    return 0;
+int main(){
+float C;
+printf("請輸入攝氏溫度：");
+scanf("%f", &C);
+double F = round((C * 9.0 / 5.0 + 32.0) * 10) / 10.0;
+// 以 float 計算 ex
+printf("%.1f℉", F);
 }
-
