af8c3a17336aee82195eb0ff8d837504097bfa79
Wei jung ting <132579810+terrywei310@users.noreply.github.com>
2023-11-06 22:22:43 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -8,10 +8,11 @@ scanf("%d", &min);
 double C;
 if (min<=800){
     C = min * 0.9;
-}else if(800 < min < 1500){
+}else if(min < 1500){
     C = min * 0.9 * 0.9;
 }else{
     C = min * 0.9 * 0.79;
 }
 printf("電話費：%.1f\n",C);
 }
+
